2a571c3bc1486dfb95404dd5b2bc66447c54fac2
Bhavana Vanjani <bhavana.vanjani@gmail.com>
2021-01-16 15:29:05 +0530

    Update heapsort.py

--- practical 1/heapsort.py
+++ practical 1/heapsort.py
@@ -36,4 +36,18 @@ print("Sorted array is")
 for i in range(n):
     print("%d" %arr[i]),
     
+Output:
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/bhavana vanjani/AppData/Local/Programs/Python/Python39/Msc CS Pracs/heapsort.py
+Sorted array is
+5
+6
+7
+11
+12
+13
+>>> 
+    
     
