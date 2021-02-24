bc9dcff6fac071e17fc04f2f563defe6afc29f22
Bhavana Vanjani <bhavana.vanjani@gmail.com>
2021-02-24 23:48:49 +0530

    Update P1_Randomized_Selection_Algorithm.py

--- P1_Randomized_Selection_Algorithm.py
+++ P1_Randomized_Selection_Algorithm.py
@@ -27,3 +27,15 @@ def RSelect(x,k):
 x = [3,1,8,4,7,9]
 for i in range(len(x)):
     print (RSelect(x,i))
+    
+ Output:
+ Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\bhavana vanjani\Documents\Msc CS Part1  Pracs\Algorithm -paper 1\P1_Randomized_Selection_Algorithm.py
+1
+3
+4
+7
+8
+9
