ebf9d0961254ba74d399269b774389d68ddbc258
Bhavana Vanjani <bhavana.vanjani@gmail.com>
2021-02-25 00:00:40 +0530

    Update P7_SetCoveringProblem.py

--- P7_SetCoveringProblem.py
+++ P7_SetCoveringProblem.py
@@ -28,6 +28,12 @@ def main():
 if __name__ == '__main__':
     main()
 
+Output:
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\bhavana vanjani\Documents\Msc CS Part1  Pracs\Algorithm -paper 1\P7_SetCoveringProblem.py
+[{1, 2, 3, 8, 9, 10}, {4, 5, 7}, {5, 6, 7}]
 
 
     
