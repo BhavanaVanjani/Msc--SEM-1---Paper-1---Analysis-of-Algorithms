a154e195fe2d601eb54cbf99ff514b62ebf6270d
Bhavana Vanjani <bhavana.vanjani@gmail.com>
2021-02-25 00:01:51 +0530

    Update P8_SubsetWithAGivenSum.py

--- P8_SubsetWithAGivenSum.py
+++ P8_SubsetWithAGivenSum.py
@@ -31,3 +31,10 @@ if (isSubsetSum(set, n, sum) == True) :
     print("Found a subset with given sum") 
 else : 
     print("No subset with given sum")
+    
+Output:
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\bhavana vanjani\Documents\Msc CS Part1  Pracs\Algorithm -paper 1\P8_SubsetWithAGivenSum.py
+Found a subset with given sum
